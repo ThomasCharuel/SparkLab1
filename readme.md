4863f4fd71108321929b07a857b0c0ad782eb7f1
Thomas Charuel <thomascharuel@hotmail.fr>
2017-10-25 19:26:49 +0200

    query 2 done

--- readme.md
+++ readme.md
@@ -3,6 +3,10 @@ Exercise: Shop Revenue Analysis
 The revenues of a souvenir shop that is present in different cities in France is given in text files.
 Each file contains the monthly income of a shop's branch in one year (each line contains a month and the corresponding revenue).
 
+Each store (shop's branch) is identified by a name as follows:
+- city_i; i=1, 2, ... Depending on the number of stores in the city
+- If only one store is present in a city X, it is identified by the name of the city
+
 The goal is to write a small Spark script that allows to display different statistics on the shop's performance:
 - Average monthly income of the shop in France (on a 1 year data)
 - Total revenue per city per year
--- totalRevenuePerCity.py
+++ totalRevenuePerCity.py
@@ -8,7 +8,22 @@ sc = SparkContext()
 # The RDD is the key/value list with key the filename and value the file content
 files = sc.wholeTextFiles(sys.argv[1])
 
-# Split the files by lines
-cities_with_lines = files.map(lambda file: file[1].splitlines())
+# Generate an object for each line on each file like ('store_name', 'city', 'month', 'income')
+f1 = files.map(lambda file: (file[0].split("/")[-1], file[1]))
+f2 = f1.map(lambda file: (file[0].split(".txt")[0], file[1]))
+f3 = f2.flatMapValues(lambda v: v.split("\r\n"))
+f4 = f3.map(lambda kv: (kv[0], kv[0].split("_")[0], kv[1].split(" ")[0], kv[1].split(" ")[1]))
+cityAsKey = f4.map(lambda scmr: (scmr[1], scmr[0], scmr[2], scmr[3]))
 
+
+# Compute the total revenue per city
+total_revenue_per_city = cityAsKey.map(lambda income_line: (income_line[0], int(income_line[3]))) \
+  .reduceByKey(lambda income1, income2: income1 + income2)
+
+# Display result
 print("Total revenue per city per year")
+
+result = total_revenue_per_city.collect()
+
+for res in result:
+  print(res)
